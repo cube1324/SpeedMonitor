702aeff2f4e6e5cadd8cbffab05d7c434b101e9d
Jonas <jonas.guenster@gmx.de>
2020-10-11 12:58:36 +0200

    added requirements.txt

--- speedmonitor.py
+++ speedmonitor.py
@@ -3,7 +3,6 @@ from pandas import DataFrame, read_csv
 from datetime import datetime
 
 time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-print(time)
 
 st = speedtest.Speedtest()
 
@@ -14,6 +13,7 @@ up = int(st.upload())
 down = int(st.download())
 
 try:
+    #Absolut path so we can use cronjobs
     old = read_csv('/home/pi/SpeedMonitor/data.csv')
 except:
     old = DataFrame([], columns=['DateTime', 'Upload', 'Download', 'Server'])
@@ -22,4 +22,5 @@ df = DataFrame([[time, up, down, server]], columns=['DateTime', 'Upload', 'Downl
 
 old = old.append(df, ignore_index=True)
 
+#Absolut path so we can use cronjobs
 old.to_csv('/home/pi/SpeedMonitor/data.csv', index=None, header=True)
